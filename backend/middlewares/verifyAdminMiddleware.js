44014fa41eb0a9411a63d4a370ce9ae2ec5afba3
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-25 22:50:51 +0530

    Minor formatting changes.

--- backend/middlewares/verifyAdminMiddleware.js
+++ backend/middlewares/verifyAdminMiddleware.js
@@ -6,32 +6,32 @@ import AdminModel from '../models/adminModel.js';
 
 const verifyAdmin = asyncHandler( async (req, res, next) => {
 
-    try {
+  try {
 
-        const decodedJwtPayload = req.currentUser;
+    const decodedJwtPayload = req.currentUser;
 
-        // Search the Db with the userId obtained after decoding jwt payload to Verify the userId claimed by JWT Payload is valid.
-        const requestUser = await AdminModel.findById(decodedJwtPayload.id).select('-password');
+    // Search the Db with the userId obtained after decoding jwt payload to Verify the userId claimed by JWT Payload is valid.
+    const requestUser = await AdminModel.findById(decodedJwtPayload.id).select('-password');
 
-        if (requestUser) {
+    if (requestUser) {
+    
+      req.user = requestUser; // Set the request user with the user data fetched from the Db
+
+      next(); // Proceed to next function as the user is authenticated as Admin
+
+    } else {
         
-            req.user = requestUser; // Set the request user with the user data fetched from the Db
-
-            next(); // Proceed to next function as the user is authenticated as Admin
-
-        } else {
-            
-            res.status(401).send({message: 'Invalid User'});
-
-        }
-
-    } catch (error) {
-        
-        res.status(400);
-
-        throw new Error(`Admin Authentication Failed - ${error.message}`);
+      res.status(401).send({message: 'Invalid User'});
 
     }
+
+  } catch (error) {
+      
+    res.status(400);
+
+    throw new Error(`Admin Authentication Failed - ${error.message}`);
+
+  }
 
 });
 
--- backend/middlewares/verifyUserMiddleware.js
+++ backend/middlewares/verifyUserMiddleware.js
@@ -1,6 +1,5 @@
 //? ===================================================== User Authentication Middleware =====================================================
 
-import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
