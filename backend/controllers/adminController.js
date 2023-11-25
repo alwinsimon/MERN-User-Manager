b43d6a60c29c4e3edc4a6249bb489b25285a9e3f
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-25 16:48:54 +0530

    Updated token creation logic.

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,325 +1,266 @@
 //? ===================================================== Admin Controller =====================================================
 
-
 // ===================== Importing necessary modules/files =====================
-import asyncHandler from 'express-async-handler';
-import AdminModel from '../models/adminModel.js';
-import generateAdminToken from '../utils/jwtConfig/adminJwtConfig/generateAdminToken.js';
-import destroyAdminToken from '../utils/jwtConfig/adminJwtConfig/destroyAdminToken.js';
+import asyncHandler from "express-async-handler";
+import AdminModel from "../models/adminModel.js";
+import generateAuthToken from "../utils/jwtHandlers/generateAuthToken.js";
+import destroyAuthToken from "../utils/jwtHandlers/destroyAuthToken.js";
 
-import { fetchAllUsers, deleteUser, updateUser } from '../utils/Helpers/adminHelpers.js';
+import {
+  fetchAllUsers,
+  deleteUser,
+  updateUser,
+} from "../utils/Helpers/adminHelpers.js";
 
-
-
-const authAdmin = asyncHandler ( async (req, res) => {
-
-    /*
+const authAdmin = asyncHandler(async (req, res) => {
+  /*
      # Desc: Auth user/set token
      # Route: POST /api/admin/auth
      # Access: PUBLIC
     */
 
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if ( !email || !password ) {
+  if (!email || !password) {
+    // If email or password is empty, return error
 
-        // If email or password is empty, return error
+    res.status(401);
 
-        res.status(401);
+    throw new Error(
+      "Email or Password is missing in the request, Admin authentication failed."
+    );
+  }
 
-        throw new Error('Email or Password is missing in the request, Admin authentication failed.');
+  // Find the user in Db with the email and password
+  const admin = await AdminModel.findOne({ email: email });
 
-    }
+  let passwordValid = false;
 
-    // Find the user in Db with the email and password
-    const admin = await AdminModel.findOne({ email: email});
+  if (admin) {
+    passwordValid = await admin.matchPassword(password);
+  }
 
-    let passwordValid = false;
-    
-    if (admin) {
+  if (passwordValid) {
+    // If user is created, send response back with jwt token
 
-        passwordValid = await admin.matchPassword(password);
+    generateAuthToken(res, admin._id, admin.email); // Middleware to Generate token and send it back in response object
 
-    }
+    const registeredAdminData = {
+      name: admin.name,
+      email: admin.email,
+    };
 
-    if ( passwordValid ) {
+    res.status(201).json(registeredAdminData);
+  }
 
-        // If user is created, send response back with jwt token
+  if (!admin || !passwordValid) {
+    // If user or user password is not valid, send error back
 
-        generateAdminToken(res, admin._id); // Middleware to Generate token and send it back in response object
+    res.status(401);
 
-        const registeredAdminData = {
-            name: admin.name,
-            email: admin.email
-        }
-
-        res.status(201).json(registeredAdminData);
-
-    } 
-    
-    if( !admin || !passwordValid ) {
-
-        // If user or user password is not valid, send error back
-
-        res.status(401);
-
-        throw new Error('Invalid Email or Password, Admin authentication failed.');
-    
-    }
-
+    throw new Error("Invalid Email or Password, Admin authentication failed.");
+  }
 });
 
-const registerAdmin = asyncHandler ( async (req, res) => {
-
-    /*
+const registerAdmin = asyncHandler(async (req, res) => {
+  /*
      # Desc: Register new user
      # Route: POST /api/admin/auth
      # Access: PUBLIC
     */
 
-    const { name, email, password, adminRegistrationKey } = req.body;
+  const { name, email, password, adminRegistrationKey } = req.body;
 
-    if ( !email || !password ) {
+  if (!email || !password) {
+    // If email or password is empty, return error
 
-        // If email or password is empty, return error
+    res.status(401);
 
-        res.status(401);
+    throw new Error(
+      "Email or Password is missing in the request, Admin registration failed."
+    );
+  }
 
-        throw new Error('Email or Password is missing in the request, Admin registration failed.');
+  if (!adminRegistrationKey) {
+    // If adminRegistrationKey is empty, return error
 
+    res.status(401);
+
+    throw new Error(
+      "No Admin Registration Access Code, Admin registration aborted."
+    );
+  } else {
+    // Check if Admin registration key is valid
+    if (process.env.ADMIN_REGISTRATION_KEY !== adminRegistrationKey) {
+      res.status(401);
+
+      throw new Error(
+        "Invalid Admin Registration Access Code, Admin registration failed."
+      );
     }
+  }
 
-    if ( !adminRegistrationKey ) {
+  // Check if user already exist
+  const userExists = await AdminModel.findOne({ email });
 
-        // If adminRegistrationKey is empty, return error
+  // If the user already exists, throw an error
+  if (userExists) {
+    res.status(400);
 
-        res.status(401);
+    throw new Error("Admin already exists.");
+  }
 
-        throw new Error('No Admin Registration Access Code, Admin registration aborted.');
+  // Store the user data to DB if the user dosen't exist already.
+  const user = await AdminModel.create({
+    name: name,
+    email: email,
+    password: password,
+  });
 
-    }else{
+  if (user) {
+    // If user is created, send response back with jwt token
 
-        // Check if Admin registration key is valid
-        if(process.env.ADMIN_REGISTRATION_KEY !== adminRegistrationKey){
+    generateAuthToken(res, user._id, user.email); // Middleware to Generate token and send it back in response object
 
-            res.status(401);
+    const registeredUserData = {
+      name: user.name,
+      email: user.email,
+    };
 
-            throw new Error('Invalid Admin Registration Access Code, Admin registration failed.');
+    res.status(201).json(registeredUserData);
+  } else {
+    // If user was NOT Created, send error back
 
-        }
+    res.status(400);
 
-    }
-
-    // Check if user already exist
-    const userExists = await AdminModel.findOne({ email });
-
-    // If the user already exists, throw an error
-    if (userExists) {
-
-        res.status(400);
-        
-        throw new Error('Admin already exists.');
-
-    }
-
-    // Store the user data to DB if the user dosen't exist already.
-    const user = await AdminModel.create({
-        name: name,
-        email: email,
-        password: password
-    });
-
-    
-    if (user) {
-
-        // If user is created, send response back with jwt token
-
-        generateAdminToken(res, user._id); // Middleware to Generate token and send it back in response object
-
-        const registeredUserData = {
-            name: user.name,
-            email: user.email
-        }
-
-        res.status(201).json(registeredUserData);
-
-    }else {
-
-        // If user was NOT Created, send error back
-
-        res.status(400);
-
-        throw new Error('Invalid user data, User registration failed.');
-    
-    }
-
-
+    throw new Error("Invalid user data, User registration failed.");
+  }
 });
 
-const logoutAdmin = asyncHandler ( async (req, res) => {
-
-    /*
+const logoutAdmin = asyncHandler(async (req, res) => {
+  /*
      # Desc: Logout user / clear cookie
      # Route: POST /api/admin/logout
      # Access: PUBLIC
     */
 
-    destroyAdminToken(res);
+  destroyAuthToken(res);
 
-    res.status(200).json({message: 'Admin Logged Out'});
-
+  res.status(200).json({ message: "Admin Logged Out" });
 });
 
-const getAdminProfile = asyncHandler ( async (req, res) => {
-
-    /*
+const getAdminProfile = asyncHandler(async (req, res) => {
+  /*
      # Desc: Get user profile
      # Route: GET /api/admin/profile
      # Access: PRIVATE
     */
 
-    const user = {
+  const user = {
+    name: req.user.name,
+    email: req.user.email,
+  };
 
-        name: req.user.name,
-        email: req.user.email
-
-    }
-
-    res.status(200).json({user});
-
+  res.status(200).json({ user });
 });
 
-const updateAdminProfile = asyncHandler ( async (req, res) => {
-
-    /*
+const updateAdminProfile = asyncHandler(async (req, res) => {
+  /*
      # Desc: Update user profile
      # Route: PUT /api/admin/profile
      # Access: PRIVATE
     */
 
-    // Find the user data with user id in the request object
-    const admin = await AdminModel.findById(req.user._id);
+  // Find the user data with user id in the request object
+  const admin = await AdminModel.findById(req.user._id);
 
-    if (admin) {
-    
-        // Update the user with new data if found or keep the old data itself.
-        admin.name = req.body.name || admin.name;
-        admin.email = req.body.email || admin.email;
+  if (admin) {
+    // Update the user with new data if found or keep the old data itself.
+    admin.name = req.body.name || admin.name;
+    admin.email = req.body.email || admin.email;
 
-        // If request has new password, update the user with the new password
-        if (req.body.password) {
+    // If request has new password, update the user with the new password
+    if (req.body.password) {
+      admin.password = req.body.password;
+    }
 
-            admin.password = req.body.password
-        
-        }
+    const updatedAdminData = await admin.save();
 
-        const updatedAdminData = await admin.save();
+    // Send the response with updated user data
+    res.status(200).json({
+      name: updatedAdminData.name,
+      email: updatedAdminData.email,
+    });
+  } else {
+    res.status(404);
 
-        // Send the response with updated user data
-        res.status(200).json({
-
-            name: updatedAdminData.name,
-            email: updatedAdminData.email
-
-        });
-
-    } else {
-
-        res.status(404);
-
-        throw new Error("Requested Admin not found.");
-
-    };
-
+    throw new Error("Requested Admin not found.");
+  }
 });
 
 const getAllUsers = asyncHandler(async (req, res) => {
+  const usersData = await fetchAllUsers();
 
-    const usersData = await fetchAllUsers();
+  if (usersData) {
+    res.status(200).json({ usersData });
+  } else {
+    res.status(404);
 
-    if(usersData){
-
-        res.status(200).json({ usersData });
-
-    }else{
-
-        res.status(404);
-
-        throw new Error("Users data fetch failed.");
-
-    }
-
+    throw new Error("Users data fetch failed.");
+  }
 });
 
-const deleteUserData = asyncHandler( async (req, res) => {
+const deleteUserData = asyncHandler(async (req, res) => {
+  const userId = req.body.userId;
 
-    const userId = req.body.userId;
+  const usersDeleteStatus = await deleteUser(userId);
 
-    const usersDeleteStatus = await deleteUser(userId);
+  if (usersDeleteStatus.success) {
+    const response = usersDeleteStatus.message;
 
-    if(usersDeleteStatus.success){
+    res.status(200).json({ message: response });
+  } else {
+    res.status(404);
 
-        const response = usersDeleteStatus.message;
+    const response = usersDeleteStatus.message;
 
-        res.status(200).json({ message:response });
-
-    }else{
-
-        res.status(404);
-
-        const response = usersDeleteStatus.message;
-
-        throw new Error(response);
-
-    }
-
+    throw new Error(response);
+  }
 });
 
+const updateUserData = asyncHandler(async (req, res) => {
+  const userId = req.body.userId;
+  const name = req.body.name;
+  const email = req.body.email;
 
-const updateUserData = asyncHandler( async (req, res) => {
+  if (!userId) {
+    res.status(404);
 
-    const userId = req.body.userId;
-    const name = req.body.name;
-    const email = req.body.email;
+    throw new Error("UserId not received in request. User update failed.");
+  }
 
-    if(!userId){
+  const userData = { userId: userId, name: name, email: email };
 
-        res.status(404);;
+  const usersUpdateStatus = await updateUser(userData);
 
-        throw new Error("UserId not received in request. User update failed.");
+  if (usersUpdateStatus.success) {
+    const response = usersUpdateStatus.message;
 
-    }
+    res.status(200).json({ message: response });
+  } else {
+    res.status(404);
 
-    const userData = {userId: userId, name: name, email: email};
-
-    const usersUpdateStatus = await updateUser(userData);
-
-    if(usersUpdateStatus.success){
-
-        const response = usersUpdateStatus.message;
-
-        res.status(200).json({ message:response });
-
-    }else{
-
-        res.status(404);;
-
-        throw new Error("User update failed.");
-
-    }
-
+    throw new Error("User update failed.");
+  }
 });
-
 
 export {
-
-    authAdmin,
-    registerAdmin,
-    logoutAdmin,
-    getAdminProfile,
-    updateAdminProfile,
-    getAllUsers,
-    deleteUserData,
-    updateUserData
-
+  authAdmin,
+  registerAdmin,
+  logoutAdmin,
+  getAdminProfile,
+  updateAdminProfile,
+  getAllUsers,
+  deleteUserData,
+  updateUserData,
 };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,227 +1,182 @@
 //? ===================================================== User Controller =====================================================
 
-
 // ===================== Importing necessary modules/files =====================
-import asyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
-import generateUserToken from '../utils/jwtConfig/userJwtConfig/generateUserToken.js';
-import destroyUserToken from '../utils/jwtConfig/userJwtConfig/destroyUserToken.js';
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateAuthToken from "../utils/jwtHandlers/generateAuthToken.js";
+import destroyAuthToken from "../utils/jwtHandlers/destroyAuthToken.js";
 
-
-
-const authUser = asyncHandler ( async (req, res) => {
-
-    /*
+const authUser = asyncHandler(async (req, res) => {
+  /*
      # Desc: Auth user/set token
      # Route: POST /api/users/auth
      # Access: PUBLIC
     */
 
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if ( !email || !password ) {
+  if (!email || !password) {
+    // If email or password is empty, return error
 
-        // If email or password is empty, return error
+    res.status(401);
 
-        res.status(401);
+    throw new Error(
+      "Email or Password is missing in the request, User authentication failed."
+    );
+  }
 
-        throw new Error('Email or Password is missing in the request, User authentication failed.');
+  // Find the user in Db with the email and password
+  const user = await User.findOne({ email: email });
 
+  let passwordValid = false;
+
+  if (user) {
+    passwordValid = await user.matchPassword(password);
+  }
+
+  if (passwordValid) {
+    // If user is created, send response back with jwt token
+
+    generateAuthToken(res, user._id, user.email); // Middleware to Generate token and send it back in response object
+
+    let registeredUserData = {
+      name: user.name,
+      email: user.email,
+    };
+
+    if (user.profileImageName) {
+      registeredUserData.profileImageName = user.profileImageName;
     }
 
-    // Find the user in Db with the email and password
-    const user = await User.findOne({ email: email});
+    res.status(201).json(registeredUserData);
+  }
 
-    let passwordValid = false;
-    
-    if (user) {
+  if (!user || !passwordValid) {
+    // If user or user password is not valid, send error back
 
-        passwordValid = await user.matchPassword(password);
+    res.status(401);
 
-    }
-
-    if ( passwordValid ) {
-
-        // If user is created, send response back with jwt token
-
-        generateUserToken(res, user._id); // Middleware to Generate token and send it back in response object
-
-        let registeredUserData = {
-            name: user.name,
-            email: user.email
-        }
-
-        if(user.profileImageName){
-
-            registeredUserData.profileImageName = user.profileImageName;
-            
-        }
-
-        res.status(201).json(registeredUserData);
-
-    } 
-    
-    if( !user || !passwordValid ) {
-
-        // If user or user password is not valid, send error back
-
-        res.status(401);
-
-        throw new Error('Invalid Email or Password, User authentication failed.');
-    
-    }
-
+    throw new Error("Invalid Email or Password, User authentication failed.");
+  }
 });
 
-const registerUser = asyncHandler ( async (req, res) => {
-
-    /*
+const registerUser = asyncHandler(async (req, res) => {
+  /*
      # Desc: Register new user
      # Route: POST /api/users/auth
      # Access: PUBLIC
     */
 
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    // Check if user already exist
-    const userExists = await User.findOne({ email });
+  // Check if user already exist
+  const userExists = await User.findOne({ email });
 
-    // If the user already exists, throw an error
-    if (userExists) {
+  // If the user already exists, throw an error
+  if (userExists) {
+    res.status(400);
 
-        res.status(400);
-        
-        throw new Error('User already exists');
+    throw new Error("User already exists");
+  }
 
-    }
+  // Store the user data to DB if the user dosen't exist already.
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
 
-    // Store the user data to DB if the user dosen't exist already.
-    const user = await User.create({
-        name: name,
-        email: email,
-        password: password
-    });
+  if (user) {
+    // If user is created, send response back with jwt token
 
-    
-    if (user) {
+    generateAuthToken(res, user._id, user.email); // Middleware to Generate token and send it back in response object
 
-        // If user is created, send response back with jwt token
+    const registeredUserData = {
+      name: user.name,
+      email: user.email,
+    };
 
-        generateUserToken(res, user._id); // Middleware to Generate token and send it back in response object
+    res.status(201).json(registeredUserData);
+  } else {
+    // If user was NOT Created, send error back
 
-        const registeredUserData = {
-            name: user.name,
-            email: user.email
-        }
+    res.status(400);
 
-        res.status(201).json(registeredUserData);
-
-    }else {
-
-        // If user was NOT Created, send error back
-
-        res.status(400);
-
-        throw new Error('Invalid user data, User registration failed.');
-    
-    }
-
-
+    throw new Error("Invalid user data, User registration failed.");
+  }
 });
 
-const logoutUser = asyncHandler ( async (req, res) => {
-
-    /*
+const logoutUser = asyncHandler(async (req, res) => {
+  /*
      # Desc: Logout user / clear cookie
      # Route: POST /api/users/logout
      # Access: PUBLIC
     */
 
-    destroyUserToken(res);
+  destroyAuthToken(res);
 
-    res.status(200).json({message: 'User Logged Out'});
-
+  res.status(200).json({ message: "User Logged Out" });
 });
 
-const getUserProfile = asyncHandler ( async (req, res) => {
-
-    /*
+const getUserProfile = asyncHandler(async (req, res) => {
+  /*
      # Desc: Get user profile
      # Route: GET /api/users/profile
      # Access: PRIVATE
     */
 
-    const user = {
+  const user = {
+    name: req.user.name,
+    email: req.user.email,
+    profileImageName: req.user.profileImageName,
+  };
 
-        name: req.user.name,
-        email: req.user.email,
-        profileImageName: req.user.profileImageName
-
-    }
-
-    res.status(200).json({user});
-
+  res.status(200).json({ user });
 });
 
-const updateUserProfile = asyncHandler ( async (req, res) => {
-
-    /*
+const updateUserProfile = asyncHandler(async (req, res) => {
+  /*
      # Desc: Update user profile
      # Route: PUT /api/users/profile
      # Access: PRIVATE
     */
 
-    // Find the user data with user id in the request object
-    const user = await User.findById(req.user._id);
+  // Find the user data with user id in the request object
+  const user = await User.findById(req.user._id);
 
-    if (user) {
-    
-        // Update the user with new data if found or keep the old data itself.
-        user.name = req.body.name || user.name;
-        user.email = req.body.email || user.email;
+  if (user) {
+    // Update the user with new data if found or keep the old data itself.
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
 
-        // If request has new password, update the user with the new password
-        if (req.body.password) {
+    // If request has new password, update the user with the new password
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
 
-            user.password = req.body.password
-        
-        }
+    if (req.file) {
+      user.profileImageName = req.file.filename || user.profileImageName;
+    }
 
-        if(req.file){
+    const updatedUserData = await user.save();
 
-            user.profileImageName = req.file.filename || user.profileImageName;
+    // Send the response with updated user data
+    res.status(200).json({
+      name: updatedUserData.name,
+      email: updatedUserData.email,
+      profileImageName: updatedUserData.profileImageName,
+    });
+  } else {
+    res.status(404);
 
-        }
-
-        const updatedUserData = await user.save();
-
-        // Send the response with updated user data
-        res.status(200).json({
-
-            name: updatedUserData.name,
-            email: updatedUserData.email,
-            profileImageName: updatedUserData.profileImageName
-
-        });
-
-    } else {
-
-        res.status(404);
-
-        throw new Error("Requested User not found.");
-
-    };
-
+    throw new Error("Requested User not found.");
+  }
 });
 
-
-
 export {
-
-    authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile
-
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
 };
