2af54ef21c2371a4e327ece842a4f05ce0a0c68d
Dr. Alwin Simon <003alwin@gmail.com>
2023-11-26 18:49:48 +0530

    Configured Error Handling.

--- backend/middlewares/verifyAdminMiddleware.js
+++ backend/middlewares/verifyAdminMiddleware.js
@@ -1,35 +1,27 @@
 //? ===================================================== Admin Authentication Middleware =====================================================
 
+import { BadRequestError } from "base-error-handler";
+
 import asyncHandler from 'express-async-handler';
 import AdminModel from '../models/adminModel.js';
 
 
 const verifyAdmin = asyncHandler( async (req, res, next) => {
 
-  try {
+  const decodedJwtPayload = req.currentUser;
 
-    const decodedJwtPayload = req.currentUser;
+  // Search the Db with the userId obtained after decoding jwt payload to Verify the userId claimed by JWT Payload is valid.
+  const requestUser = await AdminModel.findById(decodedJwtPayload.id).select('-password');
 
-    // Search the Db with the userId obtained after decoding jwt payload to Verify the userId claimed by JWT Payload is valid.
-    const requestUser = await AdminModel.findById(decodedJwtPayload.id).select('-password');
+  if (requestUser) {
+  
+    req.user = requestUser; // Set the request user with the user data fetched from the Db
 
-    if (requestUser) {
-    
-      req.user = requestUser; // Set the request user with the user data fetched from the Db
+    next(); // Proceed to next function as the user is authenticated as Admin
 
-      next(); // Proceed to next function as the user is authenticated as Admin
-
-    } else {
-        
-      res.status(401).send({message: 'Invalid User'});
-
-    }
-
-  } catch (error) {
+  } else {
       
-    res.status(400);
-
-    throw new Error(`Admin Authentication Failed - ${error.message}`);
+    throw new BadRequestError("Invalid User - Admin Authentication Failed.");
 
   }
 
